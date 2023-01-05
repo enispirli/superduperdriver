fedcbddf08d74775ab3f3145d25bc2a306b8a7f7
enispirli <en.ispirli@gmail.com>
2023-01-05 16:02:14 +0300

    Added file,note and credential entity.

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -0,0 +1,18 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+import lombok.Data;
+
+@Data
+public class File {
+    private Integer fileId;
+
+    private String fileName;
+
+    private String contentType;
+
+    private String fileSize;
+
+    private Integer userId;
+
+    private byte[] fileData;
+}
